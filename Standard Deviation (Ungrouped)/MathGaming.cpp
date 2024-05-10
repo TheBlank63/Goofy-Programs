bf676ccda2cbaee26aedebccf36919b4aa47fea5
tb63363 <46670605+TheBlank63@users.noreply.github.com>
2024-05-10 23:11:25 +0800

    Add files via upload

---
+++ Standard Deviation (Ungrouped)/MathGaming.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+void EnterToComtinue();
+
+int main() {
+    ofstream outputFile("file.txt");
+    outputFile.close();
+
+    ifstream inputFile("file.txt");
+    int arr[200];
+    int num = 0;
+
+    cout<<"Text editor will be opened, please paste all the data in the file and save it\n";
+    EnterToComtinue();
+    system("file.txt");
+
+    while (inputFile >> arr[num]) {
+        num++;
+    }
+
+    inputFile.close();
+
+    cout<<num<<endl;
+    
+    double x = 0, xp2 = 0;
+    
+    for(int i = 0; i < num; i++)
+    {
+        x += arr[i];
+        xp2 += pow(arr[i], 2);
+    }
+    
+    cout<<"x : "<<x<<endl<<"x^2 : "<<xp2<<endl;
+    double var = (xp2 - (pow(x,2)/num))/(num - 1);
+    cout<<"variance : "<<var<<endl;
+    cout<<"standard deviation : "<<sqrt(var)<<endl;
+    return 0;
+}
+
+void EnterToComtinue()
+{
+    char *a = new char;
+    cout<<"\n[PRESS ENTER TO CONTINUE]";
+    *a = getchar();
+    delete a;
+} 
